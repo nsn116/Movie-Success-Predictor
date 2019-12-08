71fcd9116f153ea3b931e2da1491e97404de1790
patrickjohncyh <patrickjohncyh@icloud.com>
2019-12-08 18:12:53 -0500

    Use random forest which achieves better results\!

--- ML/viz_pred.py
+++ ML/viz_pred.py
@@ -1,6 +1,7 @@
 import keras
 import numpy as np
 import pandas as pd
+import pickle
 
 from keras.models import load_model
 import matplotlib.pyplot as plt
@@ -21,8 +22,10 @@ if(label == 1):
 	model = load_model('models/rating_model.h5')
 	model.summary()
 elif(label==2 or label==3):
+	# model = pickle.load(open('models/revenue_model_rf.p', 'rb'))
 	model = load_model('models/revenue_model.h5')
 	model.summary()
+
 
 # Normalize Labels
 mean   = np.mean(labels[:3000])
@@ -42,11 +45,20 @@ test_feat   = input_features[3000:]
 
 out 		= model.predict(test_feat)
 
+# Use this for NN
 rev_pred = (((out*std)+mean)[:,0])
+
+# Use this for RF
+# rev_pred = (((out*std)+mean))
+
 rev_test = ((test_labels*std)+mean)
 sort_idx = np.argsort(rev_test)
+
+
+
 
 num_test_movies = rev_pred.shape[0]
 plt.scatter(np.arange(num_test_movies),rev_pred[sort_idx],c='blue')
 plt.scatter(np.arange(num_test_movies),rev_test[sort_idx],c='green')
+plt.bar(np.arange(num_test_movies),abs(rev_pred[sort_idx]-rev_test[sort_idx]),color='red')
 plt.show()
